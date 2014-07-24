158509de2acdb56e895701d107ad6761170dfc03
Michael Ferguson <mpherg@hotmail.com>
2014-07-24 08:29:06 -0600

    Customize per hostname

--- modules/people/manifests/mpherg.pp
+++ modules/people/manifests/mpherg.pp
@@ -10,12 +10,31 @@ class people::mpherg {
     source  => 'mpherg/dotfiles',
   }
 
-  include projects::all
+  notify { "Installing for host $::hostname": }
+  case $::hostname {
+    'faraday': {
+      include projects::all
+      $recovery_message = "If this Mac is found, please call 801-888-4162"
+    }
+
+    'higgs': {
+      include projects::python_dev
+      include projects::atlassian
+      $recovery_message = "If you need help using this Mac, please call Ferg at 801-819-8433"
+    }
+
+    default: {
+      $recovery_message = ""
+    }
+  }
+
 
   include eclipse::cpp
   include eclipse::java
   include firefox
   include handbrake
+  include iterm2::stable
+  include iterm2::colors::solarized_dark
   include java
   include mactex::full
   include macvim
@@ -57,9 +76,7 @@ class people::mpherg {
   }
 
   # OSX defaults module
-  osx::recovery_message {
-    'If this Mac is found, please call 801-888-4162':
-  }
+  osx::recovery_message { $recovery_message: }
   include osx::finder::show_all_on_desktop
   include osx::no_network_dsstores
   class { 'osx::global::key_repeat_delay':
