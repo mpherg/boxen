112cbe5a56c5b7100f7803f2382f78a877b9c834
Michael Ferguson <mpherg@hotmail.com>
2014-07-23 22:28:18 -0600

    Add dnscrypt-proxy

---
+++ modules/projects/manifests/dnscrypt.pp
@@ -0,0 +1,22 @@
+include homebrew
+
+# Ensure that dnscrypt-proxy is installed before we try to start the service
+package { 'dnscrypt-proxy' :
+  ensure => 'present',
+  before => Project['dnscrypt'],
+}
+
+class projects::dnscrypt {
+
+  # This is ugly, but it works. I'm sure there's a better way...
+  file { '/Library/LaunchDaemons/homebrew.mxcl.dnscrypt-proxy.plist':
+    source  => "/opt/boxen/repo/modules/projects/templates/dnscrypt-proxy/homebrew.mxcl.dnscrypt-proxy.plist",
+    group   => 'wheel',
+    notify  => Service['homebrew.mxcl.dnscrypt-proxy'],
+    owner   => 'root'
+  }
+
+  service { 'homebrew.mxcl.dnscrypt-proxy':
+    ensure  => running,
+  }
+}
