01eb98790d4cf2e47050f07012ca7f39193ee8eb
Michael Ferguson <mpherg@gmail.com>
2014-12-21 20:16:57 -0700

    Give up trying to install python packages

--- modules/people/manifests/mpherg/python_dev.pp
+++ modules/people/manifests/mpherg/python_dev.pp
@@ -1,20 +1,4 @@
 class people::mpherg::python_dev {
   require python
-
   include python
-
-  define install_python_packages {
-    exec { "Install $name":
-      command => "easy_install $name",
-      unless  => "python -c 'import $name'",
-      subscribe => Class['python'],
-    }
-  }
-
-  install_python_packages { [
-    'pygments',
-    'requests',
-    'Cheetah',
-    ]:
-  }
 }
